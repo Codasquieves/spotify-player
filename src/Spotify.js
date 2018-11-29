ade7f29016af3c91ff9052b8b1210ad8e697f1ba
Ivan Codasquieves <ivan@vizir.com.br>
2018-11-28 22:47:48 -0200

    Remoção token

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper-ivan';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQBZ5IBDO1SRWQGYfXVvl-nZdUEKvT6tygXGZAAaH5TTtL-3vygx2MdgCZTXg2OXxCmWaQ0FfKCjOTdV4AZlvuJPA4VTRIF31npggq_OVCWWsQ36InXolgwr1phTG2qewx8xhXKCOvARWrCo'
+  token: ''
 });
 
 export default spotify;
